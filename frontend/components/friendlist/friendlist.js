8635b7f99321a038da23c778b6b121872941a1a6
tawanr <tawan.rtn@gmail.com>
2024-01-19 11:03:03 +0000

    feat (FE): add status badge to friend list

--- frontend/components/friendlist/friendlist.js
+++ frontend/components/friendlist/friendlist.js
@@ -12,22 +12,46 @@ class FriendList extends HTMLElement {
     connectedCallback() {
         this.friends = [
             {
-                playerName: "abc",
+                playerName: "AAA",
                 playerId: 123,
                 avatar: "/uploads/avatars/42_Logo.png",
+                status: "offline",
+            },
+            {
+                playerName: "def",
+                playerId: 123,
+                avatar: "/uploads/avatars/42_Logo.png",
+                status: "online",
+            },
+            {
+                playerName: "zzz",
+                playerId: 123,
+                avatar: "/uploads/avatars/42_Logo.png",
+                status: "ingame",
             },
         ];
+    }
+
+    getStatusBadge(player) {
+        if (player.status === "online") {
+            return `<span class="badge bg-success">ONLINE</span>`;
+        } else if (player.status === "offline") {
+            return `<span class="badge bg-danger">OFFLINE</span>`;
+        } else if (player.status === "ingame") {
+            return `<span class="badge bg-primary">IN-GAME</span>`;
+        }
     }
 
     render(html) {
         this.shadow.innerHTML = html;
 
         const friendList = this.shadow.getElementById("friendList");
-        for (let i = 0; i < 10; i++) {
-            const friend = this.friends[0];
+        for (let i = 0; i < 8; i++) {
+            const friend = this.friends[i % 3];
             const friendItem = document.createElement("li");
             friendItem.classList.add("friendlistItem");
             friendItem.classList.add("list-group-item");
+
             friendItem.innerHTML = `
             <div class="d-flex flex-row justify-content-around w-100 h-100">
                 <div class="d-flex flex-row text-start w-100">
@@ -35,8 +59,11 @@ class FriendList extends HTMLElement {
                         <img src="${friend.avatar}" class="avatar" />
                     </div>
                     <div class="d-flex flex-column justify-content-center">
-                        <span class="date fs-4 fw-bold">${friend.playerName}</span>
+                        <span class="date fs-5">${friend.playerName}</span>
                     </div>
+                </div>
+                <div class="d-flex flex-row align-items-center friendStatus me-3">
+                    ${this.getStatusBadge(friend)}
                 </div>
                 <div class="d-flex flex-column justify-content-center">
                     <div class="d-flex flex-row">
