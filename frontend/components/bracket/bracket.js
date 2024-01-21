23687d987a154e91e893cf7097ce02b46d3ad94a
tawanr <tawan.rtn@gmail.com>
2024-01-21 12:11:21 +0000

    feat (FE): draw lines between games in bracket

--- frontend/components/bracket/bracket.js
+++ frontend/components/bracket/bracket.js
@@ -111,15 +111,38 @@ class Bracket extends HTMLElement {
         const bracket = this.shadow.getElementById("tourBracket");
 
         let currentRoundLimit = this.tournamentSize / 2 || this.bracket.length * 2;
+        let currentRoundCount = this.tournamentSize / 2 || this.bracket.length * 2;
         let bracketColumn = document.createElement("div");
         bracketColumn.classList.add("bracketColumn");
 
         for (let i = 0; i < this.tournamentSize - 1; i++) {
             if (i === currentRoundLimit) {
                 currentRoundLimit += currentRoundLimit / 2;
+                currentRoundCount /= 2;
                 bracket.appendChild(bracketColumn);
                 bracketColumn = document.createElement("div");
                 bracketColumn.classList.add("bracketColumn");
+
+                const padderLeft = document.createElement("div");
+                padderLeft.classList.add("bracketLinkLeft");
+                padderLeft.style.height = `${100 / (currentRoundCount * 2)}%`;
+                const paddingColumnLeft = document.createElement("div");
+                paddingColumnLeft.classList.add("bracketColumn");
+                for (let j = 0; j < currentRoundCount; j++) {
+                    paddingColumnLeft.appendChild(padderLeft.cloneNode(true));
+                }
+
+                const padderRight = document.createElement("div");
+                padderRight.classList.add("bracketLinkRight");
+                padderLeft.style.height = `${100 / currentRoundCount}%`;
+                const paddingColumnRight = document.createElement("div");
+                paddingColumnRight.classList.add("bracketColumn");
+                for (let j = 0; j < currentRoundCount; j++) {
+                    paddingColumnRight.appendChild(padderRight.cloneNode(true));
+                }
+
+                bracket.appendChild(paddingColumnLeft);
+                bracket.appendChild(paddingColumnRight);
             }
             const bracketGame = document.createElement("bracket-game");
             bracketGame.classList.add("bracketGame");
